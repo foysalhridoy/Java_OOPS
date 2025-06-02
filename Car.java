69d05fe8585385135ac4b356c113467b6309e9d3
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-06-02 20:11:25 +0600

    Car.java

---
+++ Car.java
@@ -0,0 +1,17 @@
+class Car {
+    String color;
+    int speed;
+
+    public void drive() {
+        System.out.println("Car is driving at speed: " + speed);
+    }
+}
+
+public class OOPS {
+    public static void main(String[] args) {
+        Car myCar = new Car(); // Creating object
+        myCar.color = "Red";
+        myCar.speed = 100;
+        myCar.drive();
+    }
+}
