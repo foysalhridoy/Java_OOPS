7eb3c07bc75030227b87d5b6769c62ef0916c151
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-06-02 20:28:58 +0600

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+//Method Overloading (Compile-Time Polymorphism)
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(2, 3));
+        System.out.println(calc.add(2.5, 3.5));
+    }
+}
