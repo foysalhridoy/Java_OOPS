bf6a0022f8d87ee5a25db898548335da0c56ad80
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-06-02 20:23:26 +0600

    Animal.java

---
+++ Animal.java
@@ -0,0 +1,20 @@
+//Inheritance
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat(); // from Animal
+        d.bark(); // from Dog
+    }
+}
